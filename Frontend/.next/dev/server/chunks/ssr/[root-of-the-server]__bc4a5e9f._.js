6c75809e79c57d9d8526ded4161be320aeae525d
abhisheksharma226 <avisheksharma2004@gmail.com>
2025-12-15 02:40:56 +0530

    minor updates

--- Frontend/.next/dev/server/chunks/ssr/[root-of-the-server]__bc4a5e9f._.js
+++ Frontend/.next/dev/server/chunks/ssr/[root-of-the-server]__bc4a5e9f._.js
@@ -1194,7 +1194,7 @@ function DashboardPreview() {
         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
             className: "bg-primary-light/50 rounded-2xl p-2 shadow-2xl",
             children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$image$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["default"], {
-                src: "/images/dashboard-preview.png",
+                src: "/images/dashboard-preview-final.png",
                 alt: "Dashboard preview",
                 width: 1160,
                 height: 700,
